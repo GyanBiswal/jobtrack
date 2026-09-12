c739da7a50c202dedf3f8684ea8abfdd25d9e677
Gyan Biswal <gyanbiswal07@gmail.com>
2026-09-12 18:39:00 +0530

    feat(frontend): React Router setup, page skeleton, Axios instance

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-brand">JobTrack</Link>
+      <div className="navbar-links">
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/JobForm.jsx
@@ -0,0 +1,5 @@
+function JobForm() {
+  return <h2>Job Form Page</h2>;
+}
+
+export default JobForm;
